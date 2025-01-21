6cb16cdfab56376afab5cbd920400bc1f86a2948
Mauriciott61 <mauriciobocanegra122@gmail.com>
2025-01-20 21:06:05 -0300

    Delete Utils.js

--- Utils.js
+++
@@ -1,8 +0,0 @@
-// utils.js
-function formatearFecha(date) {
-    // Función para formatear la fecha
-    const options = { year: 'numeric', month: 'long', day: 'numeric' };
-    return new Date(date).toLocaleDateString('es-ES', options);
-}
-
-export { formatearFecha };
